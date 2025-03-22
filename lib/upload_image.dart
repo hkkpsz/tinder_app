4dac6cf93f125fdef66be455cfc246c6f8840463
Hakkı Kapusuz <hkkpsz60@gmail.com>
2025-03-22 22:17:09 +0300

    resim ekleme ekranı oluşturulup resim ekleme tamamlandı

--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -2,13 +2,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:postgres/postgres.dart';
-import 'package:ucanble_tinder/ikon_icons.dart';
-import 'package:ucanble_tinder/main.dart';
+import 'package:ucanble_tinder/home_page.dart';
 
 class UploadImagePage extends StatefulWidget {
   final String userId; // Kullanıcı ID'sini almak için
 
-  UploadImagePage({required this.userId}); // Kullanıcı ID'si gerekli
+  UploadImagePage({required this.userId});
 
   @override
   _UploadImagePageState createState() => _UploadImagePageState();
@@ -16,32 +15,28 @@ class UploadImagePage extends StatefulWidget {
 
 class _UploadImagePageState extends State<UploadImagePage> {
   final ImagePicker _picker = ImagePicker();
-  XFile? _image;
+  List<File?> selectedImages = List.filled(6, null); // 6 buton için liste
+  bool _hasError = false; // Hata durumu kontrolü
 
   // Resim seçme fonksiyonu
-  Future<void> _pickImage() async {
+  Future<void> _pickImage(int index) async {
     final XFile? pickedImage = await _picker.pickImage(
       source: ImageSource.gallery,
     );
+
     if (pickedImage != null) {
       setState(() {
-        _image = pickedImage;
+        selectedImages[index] = File(pickedImage.path);
+        _hasError = false; // Hata durumunu sıfırla
       });
+
+      // Veritabanına kaydet
+      // await _saveImageToDatabase(widget.userId, pickedImage.path);
     }
   }
 
-  // Veritabanına resmi kaydetme fonksiyonu
-  Future<void> _saveImageToDatabase() async {
-    if (_image == null) return;
-
-    String imagePath = _image!.path; // Seçilen resmin dosya yolu
-    await _saveImagePathToDatabase(widget.userId, imagePath);
-
-    print("Gönderilen userId: ${widget.userId}");
-  }
-
-  // PostgreSQL veritabanına resim yolunu kaydetme fonksiyonu
-  Future<void> _saveImagePathToDatabase(String userId, String image_url) async {
+  // Veritabanına kaydetme fonksiyonu (isteğe bağlı olarak kullanılabilir)
+  Future<void> _saveImageToDatabase(String userId, String imageUrl) async {
     final connection = PostgreSQLConnection(
       '10.0.2.2',
       5432,
@@ -51,25 +46,110 @@ class _UploadImagePageState extends State<UploadImagePage> {
     );
 
     await connection.open();
-
-    // Firebase uid ile userId'yi eşleştiriyoruz
     await connection.query(
       'INSERT INTO image (user_id, image_url) '
-      'SELECT id, @imageUrl FROM users WHERE firebase_uid = @firebaseUid',
+          'SELECT id, @imageUrl FROM users WHERE firebase_uid = @firebaseUid',
       substitutionValues: {
-        'imageUrl': image_url,
-        'firebaseUid': userId, // userId aslında firebase_uid
+        'imageUrl': imageUrl,
+        'firebaseUid': userId,
       },
     );
 
     await connection.close();
   }
 
+  // Devam Et butonuna basıldığında yapılacak işlem
+  void _onContinuePressed() {
+    // En az 2 fotoğraf yüklenmiş mi kontrolü
+    if (selectedImages.where((image) => image != null).length < 2) {
+      setState(() {
+        _hasError = true; // Hata durumunu işaretle
+      });
+      _showErrorDialog("Devam etmek için minimum 2 fotoğraf eklemelisiniz!",); // Hata mesajı göster
+    } else {
+      // Burada fotoğrafları veritabanına gönderebilirsiniz
+      for (var i = 0; i < selectedImages.length; i++) {
+        if (selectedImages[i] != null) {
+          _saveImageToDatabase(widget.userId, selectedImages[i]!.path);
+        }
+      }
+      // İşlem tamamlandığında yönlendirme veya başka bir işlem yapılabilir
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const HomePage())); // Sayfayı kapat
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // Dialog dışına tıklayarak kapatma
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: EdgeInsets.all(20),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                message,
+                style: TextStyle(fontSize: 13, color: Colors.red),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Dialogu kapat
+                },
+                child: Text("Tamam"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+
+  // Bottom Sheet açma fonksiyonu
+  void _showImagePicker(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // Dialog dışına tıklayarak kapatma
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: EdgeInsets.all(20),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Resim Seç",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () async {
+                  Navigator.pop(context); // Önce dialog'u kapat
+                  await _pickImage(index); // Sonra resmi seç
+                },
+                icon: Icon(Icons.image),
+                label: Text("Galeriden Seç"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
-      // appBar: AppBar(title: Text("Resim Yükle")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -88,219 +168,88 @@ class _UploadImagePageState extends State<UploadImagePage> {
             ),
             Column(
               children: [
+                // İlk satır (3 buton)
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+                  children: List.generate(3, (index) => _buildImageContainer(index)),
                 ),
                 SizedBox(height: 30),
+                // İkinci satır (3 buton)
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Container(
-                      child: FloatingActionButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.add, color: Colors.white),
-                            SizedBox(height: 15),
-                            Text(
-                              "Resim Ekle",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      height: 160,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+                  children: List.generate(3, (index) => _buildImageContainer(index + 3)),
                 ),
                 SizedBox(height: 90),
                 Align(
-                  alignment: Alignment.centerRight, // Sağ tarafa yasla
+                  alignment: Alignment.centerRight,
                   child: Padding(
-                    padding: const EdgeInsets.only(right: 16.0), // Sağdan biraz boşluk bırak
-                    child: Container(
-                      height: 50,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 1),
-                        color: colorScheme.onPrimary,
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            "Devam Et",
-                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, ),
-                          ),
-                          Icon(Icons.arrow_circle_right, color: Colors.white,),
-                        ],
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: GestureDetector(
+                      onTap: _onContinuePressed, // Devam et butonuna tıklama
+                      child: Container(
+                        height: 50,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          color: colorScheme.onPrimary,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Devam Et",
+                              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                            ),
+                            Icon(Icons.arrow_circle_right, color: Colors.white),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                )
-
+                ),
               ],
             ),
           ],
         ),
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: [
-        //     if (_image != null)
-        //       Image.file(
-        //         File(_image!.path),
-        //         width: 200,
-        //         height: 200,
-        //       ),
-        //     SizedBox(height: 20),
-        //     ElevatedButton(
-        //       onPressed: _pickImage,
-        //       child: Text('Resim Seç'),
-        //     ),
-        //     SizedBox(height: 20),
-        //     ElevatedButton(
-        //       onPressed: _saveImageToDatabase,
-        //       child: Text('Resmi Yükle'),
-        //     ),
-        //   ],
-        // ),
+      ),
+    );
+  }
+
+  // Resim Container bileşeni
+  Widget _buildImageContainer(int index) {
+    return GestureDetector(
+      onTap: () => _showImagePicker(context, index),
+      child: Container(
+        height: 160,
+        width: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[300],
+          image: selectedImages[index] != null
+              ? DecorationImage(image: FileImage(selectedImages[index]!), fit: BoxFit.cover)
+              : null,
+        ),
+        child: selectedImages[index] == null
+            ? FloatingActionButton(
+          heroTag: "Resim $index",
+          onPressed: () => _showImagePicker(context, index),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.add, color: Colors.white),
+              SizedBox(height: 15),
+              Text(
+                "Resim Ekle",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                ),
+              ),
+            ],
+          ),
+        )
+            : null,
       ),
     );
   }
